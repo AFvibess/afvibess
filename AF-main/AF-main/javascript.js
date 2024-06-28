ee0226b03d87da5be2c4150114b2a235419bb587
kevincito2024 <160409331+kevincito2024@users.noreply.github.com>
2024-06-27 21:20:46 -0300

    Delete AF-main/AF-main directory

--- AF-main/AF-main/javascript.js
+++
@@ -1,7 +0,0 @@
-function scrollToSection(sectionId) {
-    const element = document.getElementById(sectionId);
-    window.scrollTo({
-        top: element.offsetTop - document.querySelector('.header').offsetHeight,
-        behavior: 'smooth'
-    });
-}
